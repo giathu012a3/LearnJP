568e747b8d4b81ae1187edb8871a3c1a1e942d68
Ntu1602 <nguyenthaitu0122@gmail.com>
2024-07-09 14:42:48 +0700

    tạo api chữ cái tiếng Nhật

--- app/Http/Controllers/AlphabetManager.php
+++ app/Http/Controllers/AlphabetManager.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Alphabet;
 use Illuminate\Http\Request;
+use Storage;
 
 class AlphabetManager extends Controller
 {
     function GetAlphabetHiragana(){
         $alphabet = Alphabet::all();
+        $alphabets = json_encode($alphabet);
+        Storage::put("alphabets_API.json",$alphabets);
         return view("include/Hiragana",["alphabet"=>$alphabet]);
     }
     function GetAlphabetKatakana(){
